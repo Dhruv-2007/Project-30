f765cfd0e1d7f36e75eb8bdb4d5746ab23e9f8e1
Dhruv-2007 <71203997+Dhruv-2007@users.noreply.github.com>
2021-01-03 20:55:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,153 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine,world;
+
+function preload(){
+  polygonimage = loadImage("polygon.png");
+}
+
+function setup() {
+  createCanvas(900,400);
+  
+   engine = Engine.create();
+   world = engine.world;
+
+    ground = new Ground(600,390,1200,20);
+    ground1 = new Ground(440,300,250,10);
+    ground2 = new Ground(750,200,200,10);
+
+    box1_1 = new Box(350,275,30,40);
+    box1_2 = new Box(380,275,30,40);
+    box1_3 = new Box(410,275,30,40);
+    box1_4 = new Box(440,275,30,40);
+    box1_5 = new Box(470,275,30,40);
+    box1_6 = new Box(500,275,30,40);
+    box1_7 = new Box(530,275,30,40);
+
+    box1_8 = new Box(380,235,30,40);
+    box1_9 = new Box(410,235,30,40);
+    box1_10 = new Box(440,235,30,40);
+    box1_11 = new Box(470,235,30,40);
+    box1_12 = new Box(500,235,30,40);
+
+    box1_13 = new Box(410,195,30,40);
+    box1_14 = new Box(440,195,30,40);
+    box1_15 = new Box(470,195,30,40);
+
+    box1_16 = new Box(440,155,30,40);
+
+    box2_1 = new Box(690,175,30,40);
+    box2_2 = new Box(720,175,30,40);
+    box2_3 = new Box(750,175,30,40);
+    box2_4 = new Box(780,175,30,40);
+    box2_5 = new Box(810,175,30,40);
+
+    box2_6 = new Box(720,135,30,40);
+    box2_7 = new Box(750,135,30,40);
+    box2_8 = new Box(780,135,30,40);
+
+    box2_9 = new Box(750,95,30,40);
+
+  polygon = Bodies.circle(150,250,20);
+  World.add(world,polygon);
+  
+  slingshot = new Slingshot(this.polygon,{x:150,y:250});
+}
+
+function draw() {
+  background("#382C2C"); 
+
+  Engine.update(engine);
+
+      ground.display();
+
+      ground1.display();
+
+      ground2.display();
+      
+      fill(" #70d1f4");
+      //Diamond Blue
+
+      box1_1.display();
+      box1_2.display();
+      box1_3.display();
+      box1_4.display();
+      box1_5.display();
+      box1_6.display();
+      box1_7.display();
+
+      fill("#f4c2c2");
+      //Baby Pink
+
+      box1_8.display();
+      box1_9.display();
+      box1_10.display();
+      box1_11.display();
+      box1_12.display();
+
+      fill("#00FFFF");
+      //Cyan
+
+      box1_13.display();
+      box1_14.display();
+      box1_15.display();
+
+      fill(" #999999");
+      //Greyish cyan
+      
+      box1_16.display();
+
+      fill("#70d1f4");
+      //Diamond Blue
+
+      box2_1.display();
+      box2_2.display();
+      box2_3.display();
+      box2_4.display();
+      box2_5.display();
+
+      fill("#00FFFF");
+      //Cyan
+
+      box2_6.display();
+      box2_7.display();
+      box2_8.display();
+
+      fill("#f4c2c2")
+      //Baby pink
+
+      box2_9.display();
+
+      fill("#00FFFF");
+      //Cyan
+      slingshot.display();
+
+      push();
+      fill("#70d1f4");
+      textSize(20);
+      stroke(3);
+      text("Press space or 'R' to get another chance!!",500 ,350);
+      text("Drag the hexagon and release the button to launch it on the towers!",30,50);
+      pop();
+  
+  imageMode(CENTER)
+  image(polygonimage ,polygon.position.x,polygon.position.y,40,40);
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+
+  slingshot.fly();
+
+}
+
+function keyPressed(){
+  if(keyCode === 32 || keyCode == 82){
+      slingshot.attach(this.polygon);
+  }
+}
